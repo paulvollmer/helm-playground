4db050aa0c1f7dff4c68d9577ddefd390010a99f
Paul Vollmer <paul.vollmer@protonmail.com>
2021-12-09 21:41:57 +0100

    moved autocomplete to Editor component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { ThemeProvider } from '@material-ui/core/styles'
 import { Box, Container, createTheme, CssBaseline, Grid, makeStyles, Typography, LinearProgress } from '@material-ui/core'
-import { addCompleter } from 'ace-builds/src-noconflict/ext-language_tools'
+import { Ace } from 'ace-builds'
 import axios from 'axios'
 
-import { Ace } from 'ace-builds'
 import Navigation from './components/Navigation'
 import RenderResult from './components/RenderResult'
 import FileViewer from './components/FileViewer'
@@ -44,114 +43,6 @@ initialTemplateSources[ingressFilename] = ingressContent
 initialTemplateSources[notesFilename] = notesContent
 initialTemplateSources[serviceFilename] = serviceContent
 initialTemplateSources[serviceaccountFilename] = serviceaccountContent
-
-const autocompleteChartYaml = [
-  {
-    name: 'apiVersion',
-    value: 'apiVersion: ',
-    caption: 'apiVersion',
-    meta: 'The chart API version (required)',
-    score: 1,
-  },
-  {
-    name: 'name',
-    value: 'name: ',
-    caption: 'name',
-    meta: 'Chart',
-    score: 1,
-  },
-  {
-    name: 'version',
-    value: 'version: ',
-    caption: 'version',
-    meta: 'A SemVer 2 version (required)',
-    score: 1,
-  },
-  {
-    name: 'kubeVersion',
-    value: 'kubeVersion: ',
-    caption: 'kubeVersion',
-    meta: 'A SemVer range of compatible Kubernetes versions (optional)',
-    score: 1,
-  },
-  {
-    name: 'description',
-    value: 'description: ',
-    caption: 'description',
-    meta: 'A single-sentence description of this project (optional)',
-    score: 1,
-  },
-  {
-    name: 'type',
-    value: 'type: ',
-    caption: 'type',
-    meta: 'The type of the chart (optional)',
-    score: 1,
-  },
-  {
-    name: 'keywords',
-    value: 'keywords:\n  - ',
-    caption: 'keywords',
-    meta: 'A list of keywords about this project (optional)',
-    score: 1,
-  },
-  {
-    name: 'home',
-    value: 'home: ',
-    caption: 'home',
-    meta: 'The URL of this projects home page (optional)',
-    score: 1,
-  },
-  {
-    name: 'sources',
-    value: 'sources:\n  - ',
-    caption: 'sources',
-    meta: 'A list of URLs to source code for this project (optional)',
-    score: 1,
-  },
-  {
-    name: 'dependencies',
-    value: 'dependencies:\n  - name: \n    version: ',
-    caption: 'dependencies',
-    meta: 'A list of the chart requirements (optional)',
-    score: 1,
-  },
-  {
-    name: 'maintainers',
-    value: 'maintainers:\n  - name: \n    email: \n    url: ',
-    caption: 'maintainers',
-    meta: '',
-    score: 1,
-  },
-  {
-    name: 'icon',
-    value: 'icon: ',
-    caption: 'icon',
-    meta: 'A URL to an SVG or PNG image to be used as an icon (optional).',
-    score: 1,
-  },
-  {
-    name: 'appVersion',
-    value: 'appVersion: ',
-    caption: 'appVersion',
-    meta: "The version of the app that this contains (optional). Needn't be SemVer. Quotes recommended.",
-    score: 1,
-  },
-  {
-    name: 'deprecated',
-    value: 'deprecated: true',
-    caption: 'deprecated',
-    meta: 'Whether this chart is deprecated (optional, boolean)',
-    score: 1,
-  },
-  {
-    name: 'annotations',
-    value: 'annotations:\n  key: value',
-    caption: 'annotations',
-    meta: '',
-    score: 1,
-  },
-]
 
 const emptySettings = {
   release: {
@@ -234,27 +125,6 @@ const App = (): JSX.Element => {
             console.error('webassembly instantiate error', err)
             setWasmError('could not instantiate helm renderer')
           })
-
-        addCompleter({
-          getCompletions(
-            editor: any, // eslint-disable-line
-            session: any,
-            pos: any,
-            prefix: any,
-            callback: (
-              arg0: null,
-              arg1: {
-                name: string
-                value: string
-                caption: string
-                meta: string
-                score: number
-              }[]
-            ) => void
-          ) {
-            callback(null, [...autocompleteChartYaml])
-          },
-        })
       })
 
     // load the golang wasm artifact and then render the react application
--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -2,12 +2,142 @@ import React, { useEffect, useState } from 'react'
 import AceEditor from 'react-ace'
 import 'ace-builds/src-noconflict/mode-yaml'
 import 'ace-builds/src-noconflict/theme-github'
+import { addCompleter } from 'ace-builds/src-noconflict/ext-language_tools'
 
 export type EditorProps = {
   annotations: any
   value: string
   onChange: any
 }
+
+const autocompleteChartYaml = [
+  {
+    name: 'apiVersion',
+    value: 'apiVersion: ',
+    caption: 'apiVersion',
+    meta: 'The chart API version (required)',
+    score: 1,
+  },
+  {
+    name: 'name',
+    value: 'name: ',
+    caption: 'name',
+    meta: 'Chart',
+    score: 1,
+  },
+  {
+    name: 'version',
+    value: 'version: ',
+    caption: 'version',
+    meta: 'A SemVer 2 version (required)',
+    score: 1,
+  },
+  {
+    name: 'kubeVersion',
+    value: 'kubeVersion: ',
+    caption: 'kubeVersion',
+    meta: 'A SemVer range of compatible Kubernetes versions (optional)',
+    score: 1,
+  },
+  {
+    name: 'description',
+    value: 'description: ',
+    caption: 'description',
+    meta: 'A single-sentence description of this project (optional)',
+    score: 1,
+  },
+  {
+    name: 'type',
+    value: 'type: ',
+    caption: 'type',
+    meta: 'The type of the chart (optional)',
+    score: 1,
+  },
+  {
+    name: 'keywords',
+    value: 'keywords:\n  - ',
+    caption: 'keywords',
+    meta: 'A list of keywords about this project (optional)',
+    score: 1,
+  },
+  {
+    name: 'home',
+    value: 'home: ',
+    caption: 'home',
+    meta: 'The URL of this projects home page (optional)',
+    score: 1,
+  },
+  {
+    name: 'sources',
+    value: 'sources:\n  - ',
+    caption: 'sources',
+    meta: 'A list of URLs to source code for this project (optional)',
+    score: 1,
+  },
+  {
+    name: 'dependencies',
+    value: 'dependencies:\n  - name: \n    version: ',
+    caption: 'dependencies',
+    meta: 'A list of the chart requirements (optional)',
+    score: 1,
+  },
+  {
+    name: 'maintainers',
+    value: 'maintainers:\n  - name: \n    email: \n    url: ',
+    caption: 'maintainers',
+    meta: '',
+    score: 1,
+  },
+  {
+    name: 'icon',
+    value: 'icon: ',
+    caption: 'icon',
+    meta: 'A URL to an SVG or PNG image to be used as an icon (optional).',
+    score: 1,
+  },
+  {
+    name: 'appVersion',
+    value: 'appVersion: ',
+    caption: 'appVersion',
+    meta: "The version of the app that this contains (optional). Needn't be SemVer. Quotes recommended.",
+    score: 1,
+  },
+  {
+    name: 'deprecated',
+    value: 'deprecated: true',
+    caption: 'deprecated',
+    meta: 'Whether this chart is deprecated (optional, boolean)',
+    score: 1,
+  },
+  {
+    name: 'annotations',
+    value: 'annotations:\n  key: value',
+    caption: 'annotations',
+    meta: '',
+    score: 1,
+  },
+]
+
+addCompleter({
+  getCompletions(
+    editor: any, // eslint-disable-line
+    session: any,
+    pos: any,
+    prefix: any,
+    callback: (
+      arg0: null,
+      arg1: {
+        name: string
+        value: string
+        caption: string
+        meta: string
+        score: number
+      }[]
+    ) => void
+  ) {
+    callback(null, [...autocompleteChartYaml])
+  },
+})
 
 // @ts-ignore
 const Editor = (props: EditorProps): JSX.Element => {
